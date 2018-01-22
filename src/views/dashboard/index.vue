f3a6d757e9980e12bfdb9f72a9d638dc087bc8d9
alien <13373935140@163.com>
2018-01-22 14:29:46 +0800

    add code

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="dashboard-container">
+    aaa
+    <!-- <component :is="currentRole"></component> -->
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'dashboard',
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+
+  }
+}
+</script>
